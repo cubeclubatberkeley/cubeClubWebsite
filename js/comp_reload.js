648b429ca75a5b17c8567da8724301a038dd5798
zodzhao <zodzhao@berkeley.edu>
2018-06-07 15:14:59 -0700

    comps page link bug

--- js/comp_reload.js
+++ js/comp_reload.js
@@ -22,7 +22,7 @@ function create_comps_obj(response) {
 		var location = response.match(/<strong>United States<\/strong>, (.*)\n/);
 		var link = response.match(/<div class="competition-link">\n.*\n.*<a href=\"(.*)\"/);
 		response = response.substring(location['index'] + 3);
-		comps.push({'dates': dates[1], 'name': name[1], 'location': location[1], 'link': "www.worldcubeassociation.com" + link[1]});
+		comps.push({'dates': dates[1], 'name': name[1], 'location': location[1], 'link': "www.worldcubeassociation.org" + link[1]});
 		i = i + 1;
 	}
 	return comps;
@@ -37,7 +37,7 @@ function make_comps_table(comp_obj){
 		table.append("<thead><tr><th>Competition Name</th><th>Dates</th><th>Location</th></tr></thead>");
 		table.append("<tbody>");
 		while(comp_num < comp_obj.length) {
-			table.append("<tr><td><a href=\"" + comp_obj[comp_num].link + "\">" + comp_obj[comp_num].name + "</a></td><td>" + comp_obj[comp_num].dates + "</td><td>" + comp_obj[comp_num].location + "</td></tr>")
+			table.append("<tr><td><a href=\"" + "https://" + comp_obj[comp_num].link + "\">" + comp_obj[comp_num].name + "</a></td><td>" + comp_obj[comp_num].dates + "</td><td>" + comp_obj[comp_num].location + "</td></tr>")
 			comp_num = comp_num + 1;
 		}
 		table.append("</tbody>")
