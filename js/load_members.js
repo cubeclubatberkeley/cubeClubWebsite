d45b1d9a5bf55b29aedb4509a5853f485f46c7f4
Abhimanyu Singhal <singhalmanu9@gmail.com>
2018-08-22 16:16:47 -0700

    load_members works, not all tooltips appear

--- js/load_members.js
+++ js/load_members.js
@@ -1,14 +1,44 @@
 $(document).ready(function() {
-	make_cards(read_csv());
+    $.ajax({
+       type: "GET",
+       url: "Cube Club Members Poll.csv",
+       dataType: "text",
+       success: function(data) {make_cards(read_csv(data));}
+    });
 });
 
-function read_csv() {
-	return [];
+function read_csv(allText) {
+    var allTextLines = allText.split(/\r\n|\n/);
+    var headers = allTextLines[0].split(',');
+    var lines = [];
+
+    for (var i=1; i<allTextLines.length; i++) {
+        var data = allTextLines[i].split(',');
+        if (data.length == headers.length) {
+
+            var tarr = {};
+            for (var j=0; j<headers.length; j++) {
+                tarr[headers[j]] = data[j];
+            }
+            lines.push(tarr);
+        }
+    }
+    // alert(lines);
+	return lines;
 }
 
 function make_cards(cards) {
-	image_row = $('.images-row');
-	for (card of cards) {
-		image_row.append('<div class="col-sm-6 col-md-4 col-lg-2 member-card"><div class="card mb3 rescard"><div class="card-body"><img class="member-picture" src="img/members/' + card[picture_name] + '"><div class="caption"><a href="https://www.worldcubeassociation.org/persons/' + card[id] + '>' + card[name] + '</a></div></div></div></div>');
+	console.log(cards);
+	image_row = $('#images-row');
+	for (var card in cards) {
+		card = cards[card];
+		image_row.append('<div class="card-ind col-sm-6 col-md-4 col-lg-2"><div class="card"><div class="card-body"><img class="card-img-top" src="img/members/' + 
+			card['\"Any image of you (make it appropriate)\"'].match("\"(.+)\"")[1] + '" data-toggle="tooltip" data-placement="left" data-selector="true" title="' + 
+			'Graduating: ' + card['\"Short Introduction (Year/Graduated Year'].match("\"(.+)")[1] + 
+			', Major:' + card[' major'] + 
+			', Favorite Puzzle/Interesting Fact:' + card[' favorite puzzle)\"'].match("(.+)\"")[1] + '"><h5 class="card-title">' + 
+			card['\"Full Name\"'].match("\"(.+)\"")[1] + '</h5><p class="card-text">WCAID: <a href="https://www.worldcubeassociation.org/persons/' + 
+			card['\"WCAID (ex. 2013SING12)\"'].match("\"(.+)\"")[1] + '">' + card['\"WCAID (ex. 2013SING12)\"'].match("\"(.+)\"")[1] + '</a></p></div></div></div>');
 	}
+    $('[data-toggle="tooltip"]').tooltip(); 
 }
